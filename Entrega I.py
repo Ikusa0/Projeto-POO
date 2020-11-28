0262b3444e852c9cd3669659d63f64eb664cffbc
Ikusa <22572595+Ikusa0@users.noreply.github.com>
2020-11-28 05:05:23 -0300

    Adicionada a função main

--- Entrega I.py
+++ Entrega I.py
@@ -1,10 +1,16 @@
 from classes.sala import Sala
 from classes.socio import Socio
 
-salinha1 = Sala("Sala de Reuniões 1", 10)
-meu_chegado1 = Socio("Irineu Pereira da Silva", "Técnico de T.I.", "003")
 
-salinha1.reservar(meu_chegado1, "20/11/2020 08:00", 50)
-salinha1.reservar(meu_chegado1, "27/11/2020 13:30", 50)
+def main():
+    salinha1 = Sala("Sala de Reuniões 1", 10)
+    meu_chegado1 = Socio("Irineu Pereira da Silva", "Técnico de T.I.", "003")
 
-print(salinha1.reservas())
+    salinha1.reservar(meu_chegado1, "20/11/2020 08:00", 50)
+    salinha1.reservar(meu_chegado1, "27/11/2020 13:30", 50)
+
+    print(salinha1.reservas())
+
+
+if __name__ == '__main__':
+    main()
